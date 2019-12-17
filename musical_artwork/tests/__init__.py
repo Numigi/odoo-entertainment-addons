9885e0e2f404ddc06edc61e78adfcd2c5cbd7f92
Jordi Riera <jordi.riera@numigi.com>
2019-12-17 17:17:38 -0500

    initial work on musical.artwork.distribution (TA#16338)

--- musical_artwork/tests/__init__.py
+++ musical_artwork/tests/__init__.py
@@ -1,8 +1,2 @@
 # © 2019 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import (
-    musical_artwork,
-    musical_artwork_role,
-    musical_artwork_distribution,
-)

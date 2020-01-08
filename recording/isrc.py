2721a3ce63d91e520eaaaa13bf0c08803d512a40
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-01-08 16:38:03 -0500

    Add 12 caracters constraint for ISRC codes (#45)

--- recording/isrc.py
+++ recording/isrc.py
@@ -6,9 +6,22 @@ from odoo.exceptions import ValidationError
 
 
 def check_isrc_code(code: str, context: dict):
+    _check_code_is_alphanumeric(code, context)
+    _check_code_has_12_chars(code, context)
+
+
+def _check_code_is_alphanumeric(code, context):
     if not code.isalnum():
         raise ValidationError(_(
             'The given ISRC ({}) is invalid. '
             'It must contain only letters and digits. '
             'No special caracters are authorized.'
+        ).format(code))
+
+
+def _check_code_has_12_chars(code, context):
+    if len(code) != 12:
+        raise ValidationError(_(
+            'The given ISRC ({}) is invalid. '
+            'It must have exactly 12 caracters.'
         ).format(code))

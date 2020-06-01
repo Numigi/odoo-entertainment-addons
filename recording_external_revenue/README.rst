61b47edcfc7d35bf94d04101cf7fc510327b41e1
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-06-01 16:13:51 -0400

    TA#20549 Add documentation and finalise raw data conversion (#72)
    * Add documentation and finalise raw data conversion

--- recording_external_revenue/README.rst
+++ recording_external_revenue/README.rst
@@ -1,24 +1,142 @@
 Recording External Revenue
 ==========================
-This module defines what is an external revenu of recording
+This module allows to import revenues from external music platforms such
+as Apple Music, Sound Exchange or Spotify.
 
-Views
------
+.. contents:: Table of Contents
 
-List view:
-~~~~~~~~~~
+Overview
+--------
+
+Raw Data
+~~~~~~~~
+The module defines a new model recording.external.revenue.raw.
 
 .. image:: static/description/raw_data_list.png
 
-Form view:
+This model is used to easily upload revenues into Odoo (as csv or xls files).
+It is designed to limit manipulations of data outside of Odoo.
+
+It contains mostly dates, strings and numeric fields.
+It has only one many2one field (company_id) used for multi-company support.
+
+Revenues
+~~~~~~~~
+The module defines another model recording.external.revenue.
+
+.. image:: static/description/revenue_list.png
+
+After raw data entries are imported in Odoo, they can be converted into (real) revenues.
+
+Basic string fields of raw revenues are mapped into many2one values.
+
+Conversion
+----------
+The conversion of raw entries into revenues is done by clicking on ``Convert Revenues``.
+
+.. image:: static/description/convert_raw_revenues_button.png
+
+Queue Jobs
 ~~~~~~~~~~
-Three tabs are defined for the form view
+The conversion is done using queue jobs.
 
-.. image:: static/description/raw_data_form.png
+One job is created per revenue.
 
-.. image:: static/description/raw_data_form_article_identifiers.png
+.. image:: static/description/queue_job_list.png
 
-.. image:: static/description/raw_data_form_revenue.png
+If a job fails, the error message will be logged inside the job.
+
+.. image:: static/description/queue_job_fail_message.png
+
+Above, the problem is a missing tax mapping entry for the label ``TPS``.
+
+Converted Revenues
+~~~~~~~~~~~~~~~~~~
+After the jobs have succeeded, the revenues will appear under ``Recording / External Revenues / Revenues``.
+
+.. image:: static/description/revenue_list_after_conversion.png
+
+Note that in our example, for 3 lines of raw data, 2 lines of revenues were created.
+
+The reason is that the system attempts to group the raw data entries together.
+
+Inside the form view of a revenue, a smart button allows to display
+the raw data lines related to this revenue.
+
+.. image:: static/description/revenue_raw_data_smart_button.png
+
+.. image:: static/description/revenue_related_raw_data_lines.png
+
+Mapping
+-------
+Here is the detail of how the raw data are converted into revenues.
+
+Mapping Tables
+~~~~~~~~~~~~~~
+The module defines mapping tables for fields converted with a simple key -> value logic.
+
+These fields include:
+
+* Country
+* Country States / Provinces
+* Currencies
+* Partner
+* Recording Platforms
+* Recording Subplatforms
+* Taxes
+
+The mapping tables for each field can be found under the ``Mapping`` menu.
+
+.. image:: static/description/mapping_menu.png
+
+Below is the mapping table for countries.
+
+.. image:: static/description/country_mapping.png
+
+If a raw data entry contains the string ``CA`` in its ``Country`` field,
+the country of the converted revenue would be ``Canada``.
+
+.. image:: static/description/raw_revenue_ca.png
+
+.. image:: static/description/converted_revenue_canada.png
+
+Mapping of Products
+~~~~~~~~~~~~~~~~~~~
+Products are mapped using a more specialized logic.
+
+.. image:: static/description/raw_revenue_product_references.png
+
+1. If the product reference is filled, it will be used to find a product with a matching internal reference (default_code).
+
+    .. image:: static/description/product_default_code.png
+
+2. Otherwise, if the external catalog reference is filled, it will be used to find the product.
+
+    .. image:: static/description/product_catalog_reference.png
+
+3. Otherwise, the type of revenue will be used to find a matching product.
+
+    .. image:: static/description/revenue_type_mapping.png
+
+    .. image:: static/description/revenue_type_mapping_streaming.png
+
+Mapping of Recordings
+~~~~~~~~~~~~~~~~~~~~~
+Recordings are also mapped using a specialized logic.
+
+1. If the product mapped at the previous section is related to a record, this record will be used.
+
+    .. image:: static/description/product_recording_relation.png
+
+2. Otherwise, if the ISRC is given, it will be used to identify the record.
+
+3. Otherwise, if the UPC code is given, it will be used.
+
+4. Otherwise, if the external catalog reference is given, it will be used.
+
+    .. image:: static/description/raw_revenue_recording_references.png
+
+    .. image:: static/description/recording_references.png
 
 Contributors
 ------------
---
+++ recording_external_revenue/tests/test_conversion.py
@@ -0,0 +1,125 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import pytest
+from ddt import ddt, data
+from odoo.exceptions import ValidationError
+from .common import ExternalRevenueCase
+
+
+@ddt
+class TestConversion(ExternalRevenueCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.company = cls.env.user.company_id
+        cls.r1 = cls._create_raw_revenue()
+        cls.r2 = cls._create_raw_revenue()
+        cls.r3 = cls._create_raw_revenue()
+        cls.raw_revenues = cls.r1 | cls.r2 | cls.r3
+
+    @classmethod
+    def _create_raw_revenue(cls, **kwargs):
+        return cls.env["recording.external.revenue.raw"].create(kwargs)
+
+    @classmethod
+    def _find_jobs(cls, revenues):
+        return (
+            cls.env["queue.job"]
+            .search(
+                [
+                    ("model_name", "=", "recording.external.revenue.raw"),
+                    ("method_name", "=", "convert"),
+                ]
+            )
+            .filtered(lambda j: set(j.record_ids) == set(revenues.ids))
+        )
+
+    @data(
+        "partner", "country", "state",
+    )
+    def test_revenues_group_by(self, field):
+        self.r1[field] = "value 1"
+        self.r2[field] = "value 2"
+        self.r3[field] = "value 1"
+        groups = self.raw_revenues.group_by_common_parameters()
+        groups = sorted(groups, key=lambda g: len(g))
+        assert len(groups) == 2
+        assert groups[0] == self.r2
+        assert groups[1] == self.r1 | self.r3
+
+    def test_schedule_conversion(self):
+        self.r1.partner = "P1"
+        self.r2.partner = "P2"
+        self.r3.partner = "P1"
+        self.env["recording.external.revenue.raw"].schedule_conversion(self.company)
+        assert len(self._find_jobs(self.r1 | self.r3)) == 1
+        assert len(self._find_jobs(self.r2)) == 1
+
+    def test_records_filtered_per_company(self):
+        other_company = self.env["res.company"].create({"name": "Other Company"})
+        self.r1.company_id = other_company
+        self.r2.company_id = other_company
+        self.env["recording.external.revenue.raw"].schedule_conversion(other_company)
+        assert len(self._find_jobs(self.r1 | self.r2)) == 1
+        assert not self._find_jobs(self.r3)
+
+    @data(
+        "commission_amount", "gross_amount", "net_amount", "quantity",
+    )
+    def test_aggregated_amounts(self, field):
+        self.r1[field] = 100
+        self.r2[field] = 200
+        self.r3[field] = 300
+        revenue = self.raw_revenues.convert()
+        assert revenue[field] == 600
+
+    def test_gross_amount_per_unit__grouped_by(self):
+        self.r1.gross_amount_per_unit = 100
+        self.r2.gross_amount_per_unit = 100
+        self.r3.gross_amount_per_unit = 200
+        self.env["recording.external.revenue.raw"].schedule_conversion(self.company)
+        assert len(self._find_jobs(self.r1 | self.r2)) == 1
+        assert len(self._find_jobs(self.r3)) == 1
+
+    def test_gross_amount_per_unit__not_summed(self):
+        self.raw_revenues.write({"gross_amount_per_unit": 100})
+        revenue = self.raw_revenues.convert()
+        assert revenue.gross_amount_per_unit == 100
+
+    def test_mapped_field(self):
+        partner_label = self.env.ref(
+            "recording_external_revenue.demo_mapping_blv"
+        ).label
+        expected_partner = self.env.ref(
+            "recording_external_revenue.demo_partner_believe"
+        )
+        self.r1.partner = partner_label
+        self.r2.partner = partner_label
+        self.r3.partner = partner_label
+        revenue = self.raw_revenues.convert()
+        assert revenue.partner_id == expected_partner
+
+    def test_raw_revenues_linked_to_revenue(self):
+        revenue = self.raw_revenues.convert()
+        assert revenue.raw_revenue_ids == self.raw_revenues
+
+    def test_raw_revenues__is_converted_flag_checked(self):
+        assert all(not r.is_converted for r in self.raw_revenues)
+        self.raw_revenues.convert()
+        assert all(r.is_converted for r in self.raw_revenues)
+
+    def test_raw_revenue_can_not_be_converted_twice(self):
+        self.raw_revenues.convert()
+        with pytest.raises(ValidationError):
+            self.raw_revenues.convert()
+
+    def test_raw_revenue_can_not_be_modified_after_conversion(self):
+        self.raw_revenues.convert()
+        with pytest.raises(ValidationError):
+            self.raw_revenues.write({"partner": "P1"})
+
+    def test_raw_revenue_can_not_be_deleted_after_conversion(self):
+        self.raw_revenues.convert()
+        with pytest.raises(ValidationError):
+            self.raw_revenues.unlink()

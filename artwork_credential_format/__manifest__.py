2490ef67c097d3a1624fd6cecd34cfce0ab83730
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2020-11-17 20:26:02 +0700

    T#006961 - PO18000173/#17767 - Évolution en cours de lot - Ajout contraintes sur référentiels

---
+++ artwork_credential_format/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Artwork Credential Format",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "website": "https://bit.ly/numigi-com",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "depends": ["musical_artwork"],
+    "data": [],
+    "installable": True,
+}

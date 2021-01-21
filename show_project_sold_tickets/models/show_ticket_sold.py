ea84ae88b42423de314e7cc2676d4a28839a4a86
hieu.vmb <hieu.vmb@komit-consulting.com>
2021-01-21 15:33:20 +0700

    [ADD]-T#007271-PO18000180-#23840-Update code review

--- show_project_sold_tickets/models/show_ticket_sold.py
+++ show_project_sold_tickets/models/show_ticket_sold.py
@@ -1,6 +1,5 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models
 
 
@@ -21,16 +20,22 @@ class ShowTicketSold(models.Model):
         readonly=True,
     )
 
+    _sql_constraints = [
+        ("record_date_uniq",
+         "UNIQUE(record_date)",
+         "This Date already exists!"),
+    ]
+
     @api.multi
     def _update_new_sold_tickets(self):
         for ticket in self:
-            previous_tickets_sold = \
+            previous_ticket_sold = \
                 self.search([
-                    ('record_date', '=', fields.Date.to_string(ticket.record_date + relativedelta(days=-1))),
+                    ('record_date', '<', ticket.record_date),
                     ('show_id', '=', ticket.show_id.id)
-                ])
-            new_sold_tickets = ticket.total_sold_tickets - sum(previous_tickets_sold.mapped("total_sold_tickets"))
-            ticket.with_context(update_new_sold_tickets=True).write({"new_sold_tickets": new_sold_tickets})
+                ], order = "record_date desc", limit=1)
+            new_sold_tickets = ticket.total_sold_tickets - previous_ticket_sold.total_sold_tickets
+            ticket.with_context(skip_update_new_sold_tickets=False).write({"new_sold_tickets": new_sold_tickets})
 
     @api.model
     def create(self, vals):
@@ -41,16 +46,15 @@ class ShowTicketSold(models.Model):
     @api.multi
     def write(self, vals):
         res = super(ShowTicketSold, self).write(vals)
-        if not self._context.get("update_new_sold_tickets", False):
+        if self._context.get("skip_update_new_sold_tickets", True):
             # Update record of next day of current modified record.
             show_tickets = self
             for ticket in self:
-                next_date_tickets_sold = \
+                next_date_ticket_sold = \
                     self.search([
                         ('show_id', '=', ticket.show_id.id),
-                        ('record_date', '=', fields.Date.to_string(
-                            ticket.record_date + relativedelta(days=1))),
-                    ])
-                show_tickets |= next_date_tickets_sold
+                        ('record_date', '>', ticket.record_date),
+                    ], order = "record_date", limit=1)
+                show_tickets |= next_date_ticket_sold
             show_tickets._update_new_sold_tickets()
         return res

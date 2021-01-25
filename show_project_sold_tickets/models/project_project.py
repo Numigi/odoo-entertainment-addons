73f4e33266d3442630c6bae4c5639347f673d8e4
hieu.vmb <hieu.vmb@komit-consulting.com>
2021-01-25 12:18:20 +0700

    [ADD]-T#007271-PO18000180-#23840-Update code review

---
+++ show_project_sold_tickets/models/project_project.py
@@ -0,0 +1,21 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
+
+    ticket_ids = fields.One2many(
+        comodel_name='show.ticket.sold',
+        inverse_name='show_id',
+    )
+
+    @api.multi
+    def _update_new_sold_tickets(self):
+        for project in self:
+            total_sold_tickets_previous = 0
+            for ticket in project.ticket_ids:
+                ticket.with_context(skip_update_new_sold_tickets=True).new_sold_tickets = \
+                    ticket.total_sold_tickets - total_sold_tickets_previous
+                total_sold_tickets_previous = ticket.total_sold_tickets
--- show_project_sold_tickets/models/show_ticket_sold.py
+++ show_project_sold_tickets/models/show_ticket_sold.py
@@ -28,62 +28,22 @@ class ShowTicketSold(models.Model):
          "A record already exists on this date for this show!"),
     ]
 
-    @api.multi
-    def _get_next_ticket_sold(self):
-        self.ensure_one()
-        next_date_ticket_sold = self.search([
-            ('show_id', '=', self.show_id.id),
-            ('record_date', '>', self.record_date),
-        ], order="record_date", limit=1)
-        return next_date_ticket_sold
-
-    @api.multi
-    def _update_new_sold_tickets(self):
-        """
-        Updates the new_sold_ticket on the current record and the next one.
-        """
-        for ticket in self:
-            previous_ticket_sold = \
-                self.search([
-                    ('record_date', '<', ticket.record_date),
-                    ('show_id', '=', ticket.show_id.id)
-                ], order = "record_date desc", limit=1)
-            new_sold_tickets = ticket.total_sold_tickets - previous_ticket_sold.total_sold_tickets
-            ticket.with_context(skip_update_new_sold_tickets=True).write(
-                {
-                    "new_sold_tickets": new_sold_tickets,
-                }
-            )
-            next_date_ticket_sold = ticket._get_next_ticket_sold()
-            if next_date_ticket_sold:
-                new_sold_tickets = next_date_ticket_sold.total_sold_tickets - ticket.total_sold_tickets
-                next_date_ticket_sold.with_context(skip_update_new_sold_tickets=True).write(
-                    {
-                        "new_sold_tickets": new_sold_tickets,
-                    }
-                )
-
-
     @api.model
     def create(self, vals):
         res = super(ShowTicketSold, self).create(vals)
-        res._update_new_sold_tickets()
+        res.show_id._update_new_sold_tickets()
         return res
 
     @api.multi
     def write(self, vals):
         res = super(ShowTicketSold, self).write(vals)
         if not self._context.get("skip_update_new_sold_tickets"):
-            self._update_new_sold_tickets()
+            self.mapped("show_id")._update_new_sold_tickets()
         return res
 
     @api.multi
     def unlink(self):
-        next_ticket_solds = self.browse()
-        for rec in self:
-            next_ticket_solds |= rec._get_next_ticket_sold()
+        projects = self.mapped("show_id")
         res = super(ShowTicketSold, self).unlink()
-        # -self to avoid "Record does not exist or has been deleted."
-        # when deleting multiple records.
-        (next_ticket_solds - self)._update_new_sold_tickets()
+        projects._update_new_sold_tickets()
         return res

a7bbc5ca29150d89282e72d4a7581a8ba9327a46
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-02-02 09:55:10 -0500

    TA#23162 manage parentality of show projects (#99)
    * T#007264 - PO18000180 - #23162 - Gestion des projets parents et types Tournée et Spectacle
    
    * [ADD]-T#007264-PO18000180-#23162-Update automatic test
    
    * [ADD]-T#007264-PO18000180-#23162-Remove redundant code
    
    * Apply 1 suggestion(s) to 1 file(s)
    
    * T#007264-PO18000180-#23162-Update code review
    
    * [ADD]-T#007264-PO18000180-#23162-Update code review
    
    * [ADD]-T#007264-PO18000180-#23162-Update code review
    
    Co-authored-by: hieu.vmb <hieu.vmb@komit-consulting.com>

--- show_project/tests/test_show_project.py
+++ show_project/tests/test_show_project.py
@@ -12,36 +12,26 @@ class TestShowProject(SavepointCase):
         super().setUpClass()
         cls.empty_project = cls.env["project.project"]
         cls.standard_project_1 = cls.env["project.project"].create(
-            {
-                "name": "Standard Project 1",
-                "show_type": "standard"}
+            {"name": "Standard Project 1", "show_type": "standard"}
         )
         cls.tour_project_1 = cls.env["project.project"].create(
-            {
-                "name": "Tour Project 1",
-                "show_type": "tour"}
+            {"name": "Tour Project 1", "show_type": "tour"}
         )
         cls.show_project_1 = cls.env["project.project"].create(
-            {
-                "name": "Show Project 1",
-                "show_type": "show"}
+            {"name": "Show Project 1", "show_type": "show"}
         )
 
     def _create_project(self, name, show_type=None, parent_id=None):
-        vals = {
-            "name": name,
-            "show_type": show_type,
-            "parent_id": parent_id,
-        }
+        vals = {"name": name, "show_type": show_type, "parent_id": parent_id}
         return self.env["project.project"].create(vals)
 
     def _update_show_date(self, project, show_date):
-        project.write({
-            "show_date": show_date
-        })
+        project.write({"show_date": show_date})
 
     def test_tour_project_dont_have_parent(self):
-        self.tour_project_2 = self._create_project("Tour Project 2", show_type="tour", parent_id=self.standard_project_1.id)
+        self.tour_project_2 = self._create_project(
+            "Tour Project 2", show_type="tour", parent_id=self.standard_project_1.id
+        )
         self.assertNotEqual(self.tour_project_2.parent_id, self.standard_project_1)
 
     def test_compute_previous_and_next_show_not_show_project(self):
@@ -61,15 +51,23 @@ class TestShowProject(SavepointCase):
         self.show_project_1.parent_id = self.tour_project_1
         self.assertEqual(self.show_project_1.previous_show_id, self.empty_project)
         self.assertEqual(self.show_project_1.next_show_id, self.empty_project)
-        self.show_project_2 = self._create_project("Show Project 2", "show", self.tour_project_1.id)
+        self.show_project_2 = self._create_project(
+            "Show Project 2", "show", self.tour_project_1.id
+        )
         self.assertEqual(self.show_project_1.previous_show_id, self.empty_project)
         self.assertEqual(self.show_project_1.next_show_id, self.empty_project)
-        self._update_show_date(self.show_project_2, fields.Date.today() - relativedelta(days=1))
+        self._update_show_date(
+            self.show_project_2, fields.Date.today() - relativedelta(days=1)
+        )
         self.assertEqual(self.show_project_1.previous_show_id, self.show_project_2)
         self.assertEqual(self.show_project_1.next_show_id, self.empty_project)
-        self.show_project_3 = self._create_project("Show Project 3", "show", self.tour_project_1.id)
+        self.show_project_3 = self._create_project(
+            "Show Project 3", "show", self.tour_project_1.id
+        )
         self.assertEqual(self.show_project_1.previous_show_id, self.show_project_2)
         self.assertEqual(self.show_project_1.next_show_id, self.empty_project)
-        self._update_show_date(self.show_project_3, fields.Date.today() + relativedelta(days=1))
+        self._update_show_date(
+            self.show_project_3, fields.Date.today() + relativedelta(days=1)
+        )
         self.assertEqual(self.show_project_1.previous_show_id, self.show_project_2)
         self.assertEqual(self.show_project_1.next_show_id, self.show_project_3)

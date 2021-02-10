950ccdcb6e8d27a8b421a5d384da05de37d10f80
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-02-10 09:32:14 -0500

    TA#23390 remove access rules on projects (#100)
    * TA#23390 remove access rules on projects
    
    * Update docker-compose.yml
    
    * Fix tests

--- show_project/tests/__init__.py
+++ show_project/tests/__init__.py
@@ -1,4 +1,2 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import test_show_project
-from . import test_show_project_security

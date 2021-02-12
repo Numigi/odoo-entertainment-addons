4d9066458f0c5800da3e1ac03d01bc999e5ba462
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-02-12 15:18:05 -0500

    Bump module versions

--- show_place/__manifest__.py
+++ show_place/__manifest__.py
@@ -2,23 +2,20 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Show Place',
-    'version': '1.0.0',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://www.numigi.com',
-    'license': 'LGPL-3',
-    'category': 'Entertainment',
-    'summary': 'Add Show Place Types/Configuration',
-    'depends': [
-        'contacts',
-        'sales_team',
+    "name": "Show Place",
+    "version": "1.1.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Entertainment",
+    "summary": "Add Show Place Types/Configuration",
+    "depends": ["contacts", "sales_team"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner.xml",
+        "views/show_place_configuration.xml",
+        "views/show_place_type.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/res_partner.xml',
-        'views/show_place_configuration.xml',
-        'views/show_place_type.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }

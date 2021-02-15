ce23025681b8698da16a5aec725095082ac7ca9e
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-02-15 15:58:14 -0500

    TA#23398 fix translations and prefetch to partner fields (#102)
    * TA#23398 fix translations and prefetch to partner fields
    
    The prefetch attribute prevents the server from crashing when updating modules.
    This is important when adding fields to res.partner because this model is used
    by the core when updating the system.
    
    * Bump module versions

--- show_project/__manifest__.py
+++ show_project/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Show Project",
     "summary": "Show Project",
-    "version": "12.0.1.0.0",
+    "version": "1.1.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
     "maintainer": "Numigi",

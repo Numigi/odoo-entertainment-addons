a8eaa9fe24185fd966e4315daa4cc819ad245094
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-09-28 16:22:27 -0400

    TA#35791 compute show project name (#121)
    * T#008208 - PO18000200 - [35791] Nom du projet en lecture seule et calculé si projet Spectacle
    
    * Increment module version
    
    Co-authored-by: cuong.nmtm <cuong.nmtm@komit-consulting.com>

--- show_project/__manifest__.py
+++ show_project/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Show Project",
     "summary": "Show Project",
-    "version": "1.1.0",
+    "version": "1.2.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
     "maintainer": "Numigi",

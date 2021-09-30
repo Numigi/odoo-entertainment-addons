1fe4e0977b4d9123d7ca3b6145fc59b8c6782e76
Gabriel <56849645+gabgosrob@users.noreply.github.com>
2021-09-30 15:27:14 -0400

    TA#38291 add crm_musical_relation module (#123)
    * add crm_musical_relation module base
    
    * complete module logic and implement views
    
    * fix docker files
    
    * add README.rst
    
    * add translation
    
    * run black
    
    * remove unused import
    
    * Update crm_musical_relation/models/crm_lead.py
    
    Co-authored-by: David Dufresne <27902736+ddufresne@users.noreply.github.com>
    
    * add ondelete to many2one in crm_lead
    
    * rename artwork dist line added field to artwork_id and adjust view
    
    * change _compute_has_rights method to iterate through recordset (self)
    
    * add unit test
    
    * fix docker-compose.yml
    
    * run black
    
    Co-authored-by: David Dufresne <27902736+ddufresne@users.noreply.github.com>

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -15,6 +15,7 @@
         "artist",
         "artist_url_link",
         "contact_birth_city",
+        "crm_musical_relation",
         "musical_artwork",
         "musical_catalog",
         "partner_organization_membership",

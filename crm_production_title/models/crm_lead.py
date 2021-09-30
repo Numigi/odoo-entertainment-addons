dc102b64f24eb2fda02da907e05132ac46b4ca02
Gabriel Gosselin Roberge <56849645+gabgosrob@users.noreply.github.com>
2021-09-30 15:48:39 -0400

    change production_type_id to related field

--- crm_production_title/models/crm_lead.py
+++ crm_production_title/models/crm_lead.py
@@ -9,10 +9,7 @@ class Lead(models.Model):
 
     production_title_id = fields.Many2one("production.title", ondelete="restrict")
     production_type_id = fields.Many2one(
-        "production.type", compute="_compute_production_type_id", readonly=True
+        "production.type",
+        related="production_title_id.production_type_id",
+        readonly=True,
     )
-
-    @api.depends("production_title_id")
-    def _compute_production_type_id(self):
-        for rec in self:
-            rec.production_type_id = rec.production_title_id.production_type_id

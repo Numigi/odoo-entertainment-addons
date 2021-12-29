7ce6b5188fd6d8be92290ca024932eee686a99a6
🌴Cuong, Nguyen Minh Tran Manh <cuong.nmtm@komit-consulting.com>
2021-12-29 19:54:52 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- show_appearance_type/__manifest__.py
+++ show_appearance_type/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Show Appearance Type",
-    "summary": "Show Project",
+    "summary": "Add new object Show Appearance Type and new field Appearance Type on Project",
     "version": "1.0.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",

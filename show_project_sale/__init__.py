0e06716391974deecdf106aa6b8bb145017037ef
Hieu.VMB <hieu.vmb@komit-consulting.com>
2022-01-27 11:07:56 +0700

    T#8948 - PO18000216 - #41069 - Bon de vente - Rappatriement données sur Projet (3)

--- show_project_sale/__init__.py
+++ show_project_sale/__init__.py
@@ -1,7 +1,4 @@
 # © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from . import sale_order
-from . import sale_order_type
-from . import project_project
-from . import show_customer_segment
+from . import models
---
+++ show_project_sale/__manifest__.py
@@ -0,0 +1,26 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Show Project Sale",
+    "version": "1.1.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Entertainment",
+    "summary": "Add sale information on project show type",
+    "depends": [
+        "show_project",
+        "show_sale",
+    ],
+    "data": [
+        # security
+        "security/ir.model.access.csv",
+        # views
+        "views/project_project.xml",
+        "views/project_service_fee.xml",
+        "views/project_ticket_price.xml",
+    ],
+    "installable": True,
+}

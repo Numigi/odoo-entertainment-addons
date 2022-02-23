e949fb3811bb951b2bcc176ea3371f2d217c285b
jbreard <37626075+jbreard@users.noreply.github.com>
2022-02-23 17:31:40 -0500

    bump version

--- show_project_sale/__manifest__.py
+++ show_project_sale/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Show Project Sale",
-    "version": "1.1.0",
+    "version": "1.1.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://www.numigi.com",

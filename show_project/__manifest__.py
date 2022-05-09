a38235091fb4597e8fa7e5736e2c72dc1409a576
Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>
2022-05-09 08:57:50 -0400

    TA#42859 bump module version (#166)

--- show_project/__manifest__.py
+++ show_project/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Show Project",
     "summary": "Show Project",
-    "version": "2.0.0",
+    "version": "2.0.1",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
     "maintainer": "Numigi",

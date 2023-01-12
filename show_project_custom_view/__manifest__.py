1ff7956b7d948c7a564f2bd8b713ff178b7fb8b2
Houda BEN TALEB <houda.bentaleb@gmail.com>
2023-01-13 00:22:41 +0100

    TA#46415 [ADD]show_project_custom_view: Create new module + change the order of the project tabs

---
+++ show_project_custom_view/__manifest__.py
@@ -0,0 +1,28 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Show Project Custom View",
+    "version": "12.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Entertainment",
+    "summary": "Modify the project view",
+    "depends": [
+        "show_place",
+        "show_project_artwork",
+        "show_project_fee",
+        "show_project_perdiem",
+        "show_project_promotion",
+        "show_project_role",
+        "show_project_sale",
+    ],
+    "data": [
+        # views
+        "views/project_project_views.xml",
+
+    ],
+    "installable": True,
+}

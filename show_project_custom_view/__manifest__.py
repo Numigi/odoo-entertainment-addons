6cc11b07a73208a29a140f73c8d223bbb95d586c
Majda EL MARIOULI <majda.elmariouli@numigi.com>
2023-03-07 15:14:29 -0500

    TA#51143 [FIX]show_project_custom_view

--- show_project_custom_view/__manifest__.py
+++ show_project_custom_view/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Show Project Custom View",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://www.numigi.com",
